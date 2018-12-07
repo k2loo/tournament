a417abe95a5ae66fd2bc13890c81810f6de7136f
Mike S <mikesonnenberg1@gmail.com>
2018-12-07 00:04:52 -0800

    added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-// scripts here
+// function toggleSpoiler() {
+
+// 	document.getElementById("btn1").classList.toggle("hidespoiler");
+
+// 	document.getElementById("btn1").classList.toggle("showspoiler");
+
+// }
+
+function toggleSpoiler() {
+	var element = document.getElementById("match1");
+	element.classList.toggle("showspoiler");
+}
